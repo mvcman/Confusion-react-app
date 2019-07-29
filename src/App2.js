afb7f5fd145115def5cf9a8b9d6280b6ecb4a9be
mandarwaghe <mandar.waghe@sweetli.me>
2019-07-29 15:41:11 +0530

    Configure with bootstrap and reactstrap

---
+++ src/App2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class App2 extends React.Component {
+  render(){
+  return (
+    <div className="App">
+      <h1>Welcome to App2 mandar!
+      </h1>
+    </div>
+  );
+}
+}
