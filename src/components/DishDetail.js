556d62d782f913607be1b70534904b272a8e1f50
mandarwaghe <mandar.waghe@sweetli.me>
2019-08-02 12:48:51 +0530

    Redux thunk added!

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 Form, FormGroup, Input, Label, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -108,7 +109,25 @@ class DishDetail extends Component{
     const dish = this.props.dish;
     console.log(dish, this.props.comments);
 
-    if(dish != null){
+    if (this.props.isLoading) {
+           return(
+               <div className="container">
+                   <div className="row">
+                       <Loading />
+                   </div>
+               </div>
+           );
+       }
+       else if (this.props.errMess) {
+           return(
+               <div className="container">
+                   <div className="row">
+                       <h4>{this.props.errMess}</h4>
+                   </div>
+               </div>
+           );
+       }
+       else if (this.props.dish != null) {
       return(
         <div class="container">
           <div className="row mt-2">
